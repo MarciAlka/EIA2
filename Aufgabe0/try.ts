7b14b9e753c9dd5cdaf00e5c96789f2371b8add3
user <user@Marci-PC.ghb.hs-furtwangen.de>
2017-10-10 19:28:32 +0200

    Aufgaben-Aktualisierung

---
+++ Aufgabe0/try.ts
@@ -0,0 +1,9 @@
+/*Aufgabe 0 von Denise Marcella Alka, 06.Oktober.2017*/
+
+
+function getName(){
+    var name = prompt("Geben Sie hier bitte ihren Namen ein:"," ");
+        document.write(name, ", Herzlich Willkommen!");      
+}
+
+
