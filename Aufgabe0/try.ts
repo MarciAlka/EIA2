e2b60a854c1c1b4867c8c3e11f883b8a70f740c5
MarciAlka <denise.marcella.alka@hs-furtwangen.de>
2017-10-11 13:48:12 +0200

    Aufgabe 0 ts

--- Aufgabe0/try.ts
+++ Aufgabe0/try.ts
@@ -1,9 +1,6 @@
 /*Aufgabe 0 von Denise Marcella Alka, 06.Oktober.2017*/
 
-
 function getName(){
     var name = prompt("Geben Sie hier bitte ihren Namen ein:"," ");
         document.write(name, ", Herzlich Willkommen!");      
 }
-
-
