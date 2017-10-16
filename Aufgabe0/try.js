1b795c92303f47c97914619bc562b7a1284b674d
MarciAlka <denise.marcella.alka@hs-furtwangen.de>
2017-10-16 14:59:06 +0200

    Verbesserung MIT map......

--- Aufgabe0/try.js
+++ Aufgabe0/try.js
@@ -1,4 +1,8 @@
-/*Aufgabe 0 von Denise Marcella Alka, 06.Oktober.2017*/
+/*Aufgabe 0 von Denise Marcella Alka, 06.Oktober.2017
+hiermit versichere ich, dass der Code von mir
+(Denise Marcella Alka, 256690)
+eigenst�ndig geschrieben wurde
+*/
 function getName() {
     var name = prompt("Geben Sie hier bitte ihren Namen ein:", " ");
     document.write(name, ", Herzlich Willkommen!");
--- Aufgabe0/try.ts
+++ Aufgabe0/try.ts
@@ -1,6 +1,10 @@
-/*Aufgabe 0 von Denise Marcella Alka, 06.Oktober.2017*/
+/*Aufgabe 0 von Denise Marcella Alka, 06.Oktober.2017
+hiermit versichere ich, dass der Code von mir 
+(Denise Marcella Alka, 256690) 
+eigenständig geschrieben wurde
+*/
 
 function getName(){
-    var name = prompt("Geben Sie hier bitte ihren Namen ein:"," ");
+    var name :string = prompt("Geben Sie hier bitte ihren Namen ein:"," ");
         document.write(name, ", Herzlich Willkommen!");      
 }
