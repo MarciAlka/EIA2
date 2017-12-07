b4220e8a3ec1284d3766422e19e1b04233ad9aef
marci <marci@admin>
2017-12-07 15:55:23 +0100

    Aufgabe 8 Quadrate erzeugen, funktionen init und eingabe (prompt benutzerfeld) 10 bis 100 Quadrate, zufällige Farbe und position

---
+++ Aufgabe8/quadrat.js
@@ -0,0 +1,62 @@
+/* Hiermit versichere ich, dass die Aufgabe 8
+von mir selbstst�ndig gemacht wurde.
+*/
+var L08_DOM;
+(function (L08_DOM) {
+    window.addEventListener("load", eingabe);
+    /*
+    function quadratDaten() {
+        
+        //Position, gr��e, Farbe
+        let pos: number;
+        let groesse: number;
+        let farbe: string;
+                
+    
+    }
+    */
+    function eingabe() {
+        //Eingabefenster f�r Benutzer
+        var anzahl = prompt("Geben Sie eine Zahl zwischen 10 und 100 ein.");
+        //Ausgabefehler sollte eine andere Zahl eingegeben werden, die nicht zwischen 1 und 100 liegt
+        //The Number.isNaN() method determines whether a value is NaN (Not-A-Number), (siehe w3school)
+        //String der eingabe in eine number umwandeln mit parseInt
+        if (Number.isNaN(parseInt(anzahl)) || parseInt(anzahl) < 10 || parseInt(anzahl) > 100) {
+            alert("falsche Eingabe, bitte Zahlen zwischen 10 und 100 eingeben!");
+            eingabe();
+        }
+        else {
+            // hier wird init aufgerufen und zuf�llige Boxen werden erzeugt, x,y, Farbe
+            for (let i = 0; i < parseInt(anzahl); i++) {
+                init(Math.random() * window.innerWidth - 80, Math.random() * window.innerHeight - 80, Math.random() * 360);
+            }
+        }
+    }
+    function init(x, y, color) {
+        //for (let i: number = 0; i < 10; i++) {
+        let div = document.createElement("div");
+        //Buchstaben f�r Farben
+        var letters = '0123456789ABCDEF';
+        div.style.width = "10px";
+        div.style.height = "10px";
+        //zuf�llige Farben
+        // vll auch so? div.style.backgroundColor = "hsl(" + color + ", 100%, 50%)";
+        //div.style.color= letters[Math.floor(Math.random() * 16)];
+        div.style.backgroundColor = "hsl(" + color + ", 100%, 50%)";
+        //Verschiebung:
+        div.style.left = x + "px";
+        div.style.top = y + "px";
+        document.body.appendChild(div);
+        /* Code von del
+        
+        let h: HTMLHeadingElement = document.createElement("h2");
+        h.innerText = "Hallo, ich bin h2-Element Nr." + i;
+        document.body.appendChild(h);
+        if (i == 3)
+            h.style.color = "red";
+        
+        */
+        //}
+    }
+})(L08_DOM || (L08_DOM = {}));
+//# sourceMappingURL=quadrat.js.map
